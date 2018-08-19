ce62db083ae9b6f5269629fb5307c7e350206f68
yuchong-pan <panyuchong@gmail.com>
2018-08-19 15:25:03 -0700

    Updated exercise solution to "boolean_functions" in Basics

--- lf/Basics.v
+++ lf/Basics.v
@@ -1242,7 +1242,19 @@ Qed.
     to the previous one but where the second hypothesis says that the
     function [f] has the property that [f x = negb x].*)
 
-(* FILL IN HERE *)
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f.
+  intros x.
+  intros b.
+  rewrite -> x.
+  rewrite -> x. destruct b.
+  - reflexivity.
+  - reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, optional (andb_eq_orb)  *)
